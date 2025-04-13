ec3b4813dad52aa81102e4efea0979c67b3fddb9
John Wang <johncwang@gmail.com>
2025-04-13 02:37:25 -0700

    feat: `rulesets`: add ruleset constants

--- api_rulesets.go
+++ api_rulesets.go
@@ -6,6 +6,14 @@ import (
 	cloudflare "github.com/cloudflare/cloudflare-go"
 )
 
+const (
+	// WAF Managed Rules - https://developers.cloudflare.com/waf/managed-rules/
+	RulesetIDCloudflareManaged                 = "efb7b8c949ac4650a09736fc376e9aee"
+	RulesetIDCloudflareOWASPCore               = "4814384a9e5d4991b9815dcfc25d2f1f"
+	RulesetIDCloudflareExposedCredentialsCheck = "c2e184081120413c86c3ab7e14069605"
+	RulesetIDCloudflareFreeManaged             = "77454fe2d30c4220b5701f6fdfb893ba"
+)
+
 type RulesetsAPI struct {
 	*cloudflare.API
 }
@@ -29,3 +37,9 @@ func (api *RulesetsAPI) ListRulesets(ctx context.Context, rc *cloudflare.Resourc
 func (api *RulesetsAPI) UpdateRuleset(ctx context.Context, rc *cloudflare.ResourceContainer, params cloudflare.UpdateRulesetParams) (cloudflare.Ruleset, error) {
 	return api.API.UpdateRuleset(ctx, rc, params)
 }
+
+/*
+	RulesetPhaseHTTPRequestFirewallCustom    RulesetPhase = "http_request_firewall_custom"
+	RulesetPhaseHTTPRequestFirewallManaged   RulesetPhase = "http_request_firewall_managed"
+	RulesetPhaseHTTPRequestLateTransform     RulesetPhase = "http_request_late_transform"
+*/
